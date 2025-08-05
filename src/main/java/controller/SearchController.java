0ad7bcde2fe4094e6d7b03ca84afb8d35707b9b3
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-05 20:46:30 +0900

    コントローラークラスの実装を追記

--- src/main/java/controller/SearchController.java
+++ src/main/java/controller/SearchController.java
@@ -1,13 +1,16 @@
 package controller;
 
+import java.util.List;
+
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import dto.SearchCondition;
+import entity.Employee;
 import service.SearchService;
 
 @Controller
@@ -22,10 +25,11 @@ public class SearchController {
 	
 	//検索機能
 	@RequestMapping("/employee/serch")
-	public String ConditionSearch(RedirectAttributes ra,
+	public String ConditionSearch(Model m,
 			                      SearchCondition condition) {
 		
-		
+		List<Employee> search = searchService.searchEmployee(condition);
+		m.addAttribute("search", search);
 		
 		return "searchEmployee";
 	}
