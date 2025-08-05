ef78a5f56dc1443c2c61e6340f7b8bfcf84a9c22
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-05 20:46:03 +0900

    範囲チェックの削除

--- src/main/java/service/SearchService.java
+++ src/main/java/service/SearchService.java
@@ -16,25 +16,6 @@ public class SearchService {
 	private SearchMapper searchMapper;
 	
 	public List<Employee> searchEmployee(SearchCondition condition) {
-		
-		//年齢の範囲チェック
-		if(condition.getMinAge() != null && condition.getMaxAge() != null && 
-		   condition.getMinAge() > condition.getMaxAge()) {
-			throw new IllegalArgumentException("年齢の下限は上限以下にしてください");
-		}
-		
-		//開始日の範囲チェック
-		if(condition.getMinStartDate() != null && condition.getMaxStartDate() != null &&
-		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			throw new IllegalArgumentException("開始日の下限は上限以下にしてください");
-		}
-		
-		//終了日の範囲チェック
-		if(condition.getMinEndDate() != null && condition.getMaxEndDate() != null &&
-		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			throw new IllegalArgumentException("終了日の下限は上限以下にしてください");
-		}
-		
 		return searchMapper.searchEmployee(condition);
 	}
 }
