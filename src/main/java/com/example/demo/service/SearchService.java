0cf00eb98dbc6b4ca3b8da532e8fc57ba20357d4
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-12 14:17:49 +0900

    バリデーションチェックを追加

--- src/main/java/com/example/demo/service/SearchService.java
+++ src/main/java/com/example/demo/service/SearchService.java
@@ -15,7 +15,55 @@ public class SearchService {
 	@Autowired
 	private SearchMapper searchMapper;
 	
+	//バリデーションチェック
+	public String validationCheck(SearchCondition condition) {
+		
+		//IDの数値チェック(入力されたidがnullではない且つ数値が0未満のとき)
+		if(condition.getId() != null && condition.getId() < 0) {
+			return "社員IDは正の数値を入力してください";
+		}
+		
+		
+		//年齢の数値チェック（入力されたminAgeとmaxAgeがnullではない且つ数値が0未満のとき）
+		if(condition.getMinAge() != null && condition.getMinAge() < 0) {
+			return "年齢は正の数値を入力してください";
+		}
+		
+		
+		if(condition.getMaxAge() != null && condition.getMaxAge() < 0) {
+			return "年齢は正の数値を入力してください";
+		}
+		
+		
+		//年齢の範囲チェック（入力されたminAgeとmaxAgeがnullではない且つminAge>maxAgeのとき）
+		if(condition.getMinAge() != null && condition.getMaxAge() != null && 
+		   condition.getMinAge() > condition.getMaxAge()) {
+			return "年齢の最小値が最大値を上回らないようにしてください";
+		}
+		
+		
+		//開始日の範囲チェック（入力されたstartmMinDateとstartMaxDateがnullではない且つstartmMinDate>startMaxDateのとき）
+		if(condition.getMinStartDate() != null && condition.getMaxStartDate() != null &&
+		   condition.getMinStartDate().isAfter(condition.getMaxStartDate())) {
+			return "開始日の最小値が最大値を上回らないようにしてください";
+		}
+		
+		
+		//終了日の範囲チェック（入力されたendmMinDateとendMaxDateがnullではない且つendMinDate>endMaxDateのとき）
+		if(condition.getMinEndDate() != null && condition.getMaxEndDate() != null &&
+		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
+			return "終了日の最小値が最大値が上回らないようにしてください";
+		}
+		
+		
+		//エラーなし
+		return null;
+	}
+	
+	
+	//検索処理
 	public List<Employee> searchEmployee(SearchCondition condition) {
 		return searchMapper.searchEmployee(condition);
 	}
+	
 }
