a38a865aab6a599bc4f72e6bdc5af285e4e5b4ed
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-12 10:39:28 +0900

    パッケージの場所を移動

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.example.demo.controller;
 
 import java.util.List;
 
@@ -7,11 +7,12 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import dto.SearchCondition;
-import entity.Employee;
-import service.SearchService;
+import com.example.demo.dto.SearchCondition;
+import com.example.demo.entity.Employee;
+import com.example.demo.service.SearchService;
 
 @Controller
 public class SearchController {
@@ -24,30 +25,34 @@ public class SearchController {
 	
 	
 	//検索機能
-	@RequestMapping("/employee/serch")
-	public String ConditionSearch(Model m,
-			                      SearchCondition condition) {
+	@GetMapping("/employee/search")
+	public String ConditionSearch(@ModelAttribute SearchCondition condition, Model m) {
 		
 		//年齢の範囲チェック
 		if(condition.getMinAge() != null && condition.getMaxAge() != null && 
 		   condition.getMinAge() > condition.getMaxAge()) {
-			
+		   m.addAttribute("errorMessage", "年齢の最大値が最小値を超えないようにしてください");
 		}
 		
 		//開始日の範囲チェック
 		if(condition.getMinStartDate() != null && condition.getMaxStartDate() != null &&
 		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			
+			m.addAttribute("errorMessage", "開始日の最大値が最小値を超えないようにしてください");
 		}
 		
 		//終了日の範囲チェック
 		if(condition.getMinEndDate() != null && condition.getMaxEndDate() != null &&
 		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			
+			m.addAttribute("errorMessage", "終了日の最大値が最小値を超えないようにしてください");
 		}
+		
 		
 		List<Employee> search = searchService.searchEmployee(condition);
 		m.addAttribute("search", search);
+		
+		
+		//セッションのユーザー情報を取得（ログインのコントローラーができたら追加）
+		
 		
 		return "searchEmployee";
 	}
