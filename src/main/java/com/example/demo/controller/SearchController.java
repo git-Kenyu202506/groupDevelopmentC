8e6f86be70849633679c9c32897c85ce6785b6dc
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-12 14:16:00 +0900

    入力チェックの処理を追加

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -7,8 +7,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.dto.SearchCondition;
 import com.example.demo.entity.Employee;
@@ -24,31 +24,22 @@ public class SearchController {
 	private HttpSession session;
 	
 	
-	//検索機能
-	@GetMapping("/employee/search")
+	@RequestMapping("/employee/search")
 	public String ConditionSearch(@ModelAttribute SearchCondition condition, Model m) {
 		
-		//年齢の範囲チェック
-		if(condition.getMinAge() != null && condition.getMaxAge() != null && 
-		   condition.getMinAge() > condition.getMaxAge()) {
-		   m.addAttribute("errorMessage", "年齢の最大値が最小値を超えないようにしてください");
+		//入力チェック
+		String errorMessage = searchService.validationCheck(condition);
+		if(errorMessage != null) {
+			m.addAttribute("errorMessage", errorMessage);
+			m.addAttribute("searchCondition", condition); //入力内容を保持
+			
+			return "searchEmployee";
 		}
 		
-		//開始日の範囲チェック
-		if(condition.getMinStartDate() != null && condition.getMaxStartDate() != null &&
-		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			m.addAttribute("errorMessage", "開始日の最大値が最小値を超えないようにしてください");
-		}
-		
-		//終了日の範囲チェック
-		if(condition.getMinEndDate() != null && condition.getMaxEndDate() != null &&
-		   condition.getMinEndDate().isAfter(condition.getMaxEndDate())) {
-			m.addAttribute("errorMessage", "終了日の最大値が最小値を超えないようにしてください");
-		}
-		
-		
+		//検索機能
 		List<Employee> search = searchService.searchEmployee(condition);
 		m.addAttribute("search", search);
+		m.addAttribute("searchCondition", condition); //入力内容を保持
 		
 		
 		//セッションのユーザー情報を取得（ログインのコントローラーができたら追加）
