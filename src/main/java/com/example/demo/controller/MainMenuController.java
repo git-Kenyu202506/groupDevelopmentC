c1c5578c9f83bbd2cedec337d745914f34a31d36
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-19 20:06:59 +0900

    MainMenuControllerを作成

---
+++ src/main/java/com/example/demo/controller/MainMenuController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainMenuController {
+
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("/employee/mainMenu")
+	public String showMainMenu(Model m) {
+		
+		//ログイン情報のセッションを取得
+		String id = (String) session.getAttribute("id");
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+		
+		m.addAttribute("id", id);
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+		
+		return "mainMenu";
+	}
+	
+	
+	//社員情報登録画面へ遷移
+	@GetMapping("/employee/register")
+	public String showRegister() {
+		return "register";
+	}
+	
+	//社員情報削除（入力）画面へ遷移
+	@GetMapping("/employee/deleteInsert")
+	public String showDelete() {
+		return "deleteInsert";
+	}
+	
+	//社員情報更新（社員ID入力）画面
+	@GetMapping("/employee/update")
+	public String showUpdate() {
+		return "updateInsert";
+	}
+}
