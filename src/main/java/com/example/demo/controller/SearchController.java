b7a7345d6c6bd99fdf3fdf093ad33fd0895c4672
kenta sugahara <kenta.sugahara.23@gmail.com>
2025-08-20 14:21:56 +0900

    GETマッピングに変更

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,7 @@ public class SearchController {
 		
 		m.addAttribute("name", name);
 		m.addAttribute("loginDateTime", loginDateTime);
+		
 		
 		//全ての検索条件がnullか判定
 		boolean isFirstAccess = (condition.getId() == null &&
@@ -83,7 +85,7 @@ public class SearchController {
 	
 	
 	//社員情報更新画面へIDを渡して遷移する機能
-	@PostMapping("/employee/update/{id}")
+	@GetMapping("/employee/update/{id}")
 	public String passId(@PathVariable("id") int id, Model m) {
 		Employee update = searchService.selectById(id);
 		
