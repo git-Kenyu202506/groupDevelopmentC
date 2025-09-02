53dfac6ab5410912c5e02fe8cbd908a528fe52fe
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-02 21:09:41 +0900

    RegisterMapperの作成 追加

---
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller;
+
+import java.time.LocalDate;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// http://localhost:8080/insertForm
+
+@Controller
+public class RegisterController {
+	@Autowired
+	private DummyService service;
+	@Autowired
+	private HttpSession session;
+
+	//CheckクラスからEmployeeクラスに参照先を変更
+
+	@RequestMapping("/DummyMain")
+	public String dummyMain() {
+		return "DummyMain";
+	}
+//登録情報入力画面
+	@RequestMapping("/insertForm")
+	public String insertForm(Model model) {
+		model.addAttribute("Employee", new Employee());
+		return "insertForm";
+	}
+//入力された情報の確認画面
+	@PostMapping("/insert_check")
+	public String insert_check(@ModelAttribute("Employee") @Validated Employee employee,
+			BindingResult bindingResult,
+			Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "insertForm";
+		}
+// パスワードのフォーマット確認
+		if (!employee.isPasswordPatternValid()) {
+			model.addAttribute("passwordFormError", "パスワードは半角英数字を含めてください");
+			return "insertForm";
+		}
+// パスワードの一致確認
+		if (!employee.isPasswordMatching()) {
+			model.addAttribute("passwordUnmatch", "パスワードが一致しません。");
+			return "insertForm";
+		}
+		return "insert_check";
+	}
+
+	//DTOを使用
+	@PostMapping("/insert")
+	public String insertEmployeee(@ModelAttribute Employee employee) {
+		if (employee.getStartDate() == null) {
+			employee.setStartDate(LocalDate.now());
+		}
+		service.insert(employee);
+		return "insert_result";
+	}
+}
