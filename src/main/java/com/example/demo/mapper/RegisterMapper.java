2d227bf8b4436ec1a7d7a9e3b549152b697046f9
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-02 21:10:00 +0900

    RegisterMapperの作成 追加

---
+++ src/main/java/com/example/demo/mapper/RegisterMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RegisterMapper {
+	
+	@Insert("INSERT INTO employee (name,age,password,startDate,endDate) VALUES (#{name},#{age},#{password},#{startDate},#{endDate})")
+	void insert(Employee employee);
+
+}
