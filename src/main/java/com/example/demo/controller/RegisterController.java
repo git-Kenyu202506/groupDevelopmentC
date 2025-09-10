3cae9c79e567dcdb58e7530de4cc2634fff8d0cb
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-11 08:51:09 +0900

    ダミーのログイン画面で入力した情報をセッションにて取得 コントローラにてバリデーションチェックのメソッドを追加 MainMenuへ転移するためのMappingを追加

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,37 +24,116 @@ import com.example.demo.service.RegisterService;
 public class RegisterController {
 	@Autowired
 	private RegisterService service;
-	@Autowired
-	private HttpSession session;
+	//フィールドに@Autowiredするとnullになりやすい
+
+	// 社員情報登録(入力)へ転移
+	@RequestMapping("/insertForm")
+	public String insertForm(Model m, HttpSession session) {
+
+		//Integer id = (Integer) session.getAttribute("id");
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+		m.addAttribute("Employee", new Employee());
+		return "insertForm";
+	}
 
 	//入力された情報の確認画面
 	@PostMapping("/insert_check")
 	public String insert_check(@ModelAttribute("Employee") @Validated Employee employee,
 			BindingResult bindingResult,
-			Model model) {
+			HttpSession session,
+			Model m) {
+
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
 
 		if (bindingResult.hasErrors()) {
 			return "insertForm";
 		}
+		//社員名の入力チェック
+		if (!service.isNameInputValid(employee)) {
+			m.addAttribute("nameNull", "社員名を入力してください");
+			return "insertForm";
+		}
+		//年齢の入力チェック&有効な数字かのチェック
+		if (!service.isAgeInputValid(employee)) {
+			m.addAttribute("ageNull", "有効な年齢を入力してください");
+			return "insertForm";
+		}
 		// パスワードのフォーマット確認
 		if (!service.isPasswordPatternValid(employee)) {
-			model.addAttribute("passwordFormError", "パスワードは半角英数字を含めてください");
+			m.addAttribute("passwordFormError", "パスワードは半角英数字を含めてください");
 			return "insertForm";
 		}
 		// パスワードの一致確認
 		if (!service.isPasswordMatching(employee)) {
-			model.addAttribute("passwordUnmatch", "パスワードが一致しません。");
+			m.addAttribute("passwordUnmatch", "パスワードが一致しません。");
 			return "insertForm";
 		}
 		return "insert_check";
 	}
 
+	// 登録完了画面へ転移(登録した日を取得)
 	@PostMapping("/insert")
-	public String insertEmployeee(@ModelAttribute Employee employee) {
+	public String insertEmployeee(@ModelAttribute Employee employee, HttpSession session, Model m) {
 		if (employee.getStartDate() == null) {
 			employee.setStartDate(LocalDate.now());
 		}
+
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
 		service.insert(employee);
 		return "insert_result";
+	}
+
+	// メインメニュー画面に転移(菅原さんファイル参照)
+	@GetMapping("/backToMainMenu")
+	public String backToMainMenu(Model m, HttpSession session) {
+
+		//ログイン情報のセッションを取得
+		Integer id = (Integer) session.getAttribute("id");
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+		return "mainMenu";
+	}
+	
+	// メインメニュー画面に転移(菅原さんファイル参照)
+	@RequestMapping("/backMainMenu")
+	public String backMainMenu(Model m, HttpSession session) {
+
+		//ログイン情報のセッションを取得
+		Integer id = (Integer) session.getAttribute("id");
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+		return "mainMenu";
+	}
+
+	// 検索画面に転移(菅原さんファイル参照)
+	@GetMapping("/searchEmployee")
+	public String ConditionSearch(HttpSession session, Model m) {
+
+		//ログイン情報のセッションを取得
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+
+		return "searchEmployee";
 	}
 }
