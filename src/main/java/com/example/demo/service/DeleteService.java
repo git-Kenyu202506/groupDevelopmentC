ee9e267688824b52aafad8dd27d19bd941132189
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-13 21:20:12 +0900

    パターンエラーのメソッドの追加

--- src/main/java/com/example/demo/service/DeleteService.java
+++ src/main/java/com/example/demo/service/DeleteService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Employee;
 import com.example.demo.mapper.DeleteMapper;
 
 @Service
@@ -12,5 +13,27 @@ public class DeleteService {
 
 	public void delete(int id) {
 		mapper.delete(id);
+	}
+
+	public boolean nullDeleteError(Employee employee) {
+		//変更予定
+		return employee.getId() > 0;
+	}
+
+	public boolean recordEmptyDeleteError(Employee employee) {
+		//テーブルの中に一致するものを探してnullならエラー 
+		if (employee == null || employee.getId() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	//
+	public boolean loginUserDeleteError(Employee employee, Employee sessionUser) {
+		//sessionにある値と照合し一致すればfalseを返す
+		if (employee != null && sessionUser != null) {
+			return employee.getId() == (sessionUser.getId());
+		}
+		return false;
 	}
 }
