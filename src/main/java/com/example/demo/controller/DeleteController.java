8222624474cb4e1f626264a9d669d2206415f0b2
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-16 12:19:21 +0900

    session部分の文を追加 エラーメソッドの追加

--- src/main/java/com/example/demo/controller/DeleteController.java
+++ src/main/java/com/example/demo/controller/DeleteController.java
@@ -28,33 +28,54 @@ public class DeleteController {
 	//削除するID確認
 	@PostMapping("/delete_check")
 	public String delete_check(@Validated Employee employee,
-			BindingResult bindingResult, Model model) {
+			BindingResult bindingResult, HttpSession session, Model m) {
+
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+
 		//入力エラーがあれば入力フォームに転移
 		if (bindingResult.hasErrors()) {
 			return "deleteForm";
 		}
+		if (!service.nullDeleteError(employee)) {
+			m.addAttribute("idnull", "IDを入力してください");
+			return "deleteForm";
+		}
 		// 入力されたIDが存在しないエラー
+		if (!service.recordEmptyDeleteError(employee)) {
+			m.addAttribute("recordEmptyDeleteError", "該当のレコードがありません。");
+			return "deleteForm";
+		}
 
 		//		if (!employee.isIdPatternValid()) {
 		//			model.addAttribute("idFormError", "入力されたIDの情報が存在しません");
 		//			return "deleteForm";
 		//		}
 		// ログインしているユーザーの情報は削除できない ログイン中のid情報を取得し
-		// if( lodinしてるUserのインスタンス != null &&  !employee.isDeleteLodinUserError()){
-		//		model.addAttribute("delete_usererror", "ログイン中のユーザーは削除できません");
-		//		return "deleteForm";
-		//}
 
-		model.addAttribute("Employee", employee);
+		//		if (service.loginUserDeleteError(employee.getId(),sessionUser.getId())) {
+		//			m.addAttribute("recordEmptyDeleteError", "ログイン中のユーザーは削除できません。");
+		//			return "deleteForm";
+		//		}
 		return "delete_check";
 	}
 
 	// 削除完了画面に転移
 	@PostMapping("/delete")
-	public String deleteEmployee(Model model, @RequestParam("id") String id) {
+	public String deleteEmployee(Model m, HttpSession session, @RequestParam("id") String id) {
+
+		String name = (String) session.getAttribute("name");
+		String loginDateTime = (String) session.getAttribute("loginDateTime");
+
+		m.addAttribute("name", name);
+		m.addAttribute("loginDateTime", loginDateTime);
+
 		int numId = Integer.parseInt(id);
 		service.delete(numId);
-		model.addAttribute("msg", "社員情報の削除が完了しました");
+		m.addAttribute("msg", "社員情報の削除が完了しました");
 		return "delete_result";
 	}
 
