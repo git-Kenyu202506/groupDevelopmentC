4f33a4ecc3c7b0df573d75164c016570073aa366
大庭 舜輔 <shunsuke1127-dance@au.com>
2025-09-18 23:30:18 +0900

    MapperにID検索の文を追加

--- src/main/java/com/example/demo/mapper/DeleteMapper.java
+++ src/main/java/com/example/demo/mapper/DeleteMapper.java
@@ -2,9 +2,15 @@ package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.entity.Employee;
 
 @Mapper
 public interface DeleteMapper {
 	@Delete("DELETE FROM employee WHERE id = #{id}")
-	void delete (int id);
+	void delete(int id);
+
+	@Select("SELECT * FROM employee WHERE id = #{id}")
+	Employee findById(int id);
 }
